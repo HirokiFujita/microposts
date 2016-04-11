bff50110b117859d57f0d18dea5d2fd924d96386
Hiroki_Fujita <hf.tkbk.japan@gmail.com>
2016-04-11 23:41:30 +0000

    user-profile_editupdate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411195943) do
+ActiveRecord::Schema.define(version: 20160411200700) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160411195943) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.text     "profile"
+    t.string   "area"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
